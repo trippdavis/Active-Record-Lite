8949428cf29b46d737abe9de9ab09066a855a422
Tripp Davis <trippdavis3@gmail.com>
2015-03-06 11:08:20 -0800

    added getter and setter methods for table_name

--- lib/01_sql_object.rb
+++ lib/01_sql_object.rb
@@ -12,11 +12,12 @@ class SQLObject
   end
 
   def self.table_name=(table_name)
-    # ...
+    @table_name = table_name
   end
 
   def self.table_name
-    # ...
+    return @table_name if @table_name
+    self.to_s.tableize
   end
 
   def self.all
