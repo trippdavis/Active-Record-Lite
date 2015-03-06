e846748e98d2515807f153d03ae7f538c17362cf
Tripp Davis <trippdavis3@gmail.com>
2015-03-06 10:55:28 -0800

    make my_attr_accessor

--- lib/00_attr_accessor_object.rb
+++ lib/00_attr_accessor_object.rb
@@ -1,5 +1,13 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
-    # ...
+    names.each do |attr|
+      define_method("#{attr}") do
+        instance_variable_get("@#{attr}")
+      end
+
+      define_method("#{attr}=") do |set_name|
+        instance_variable_set("@#{attr}", set_name)
+      end
+    end
   end
 end
