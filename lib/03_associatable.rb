ce158a5cbe9c8f1ef4cdc51c384bfe316846e633
Tripp Davis <trippdavis3@gmail.com>
2015-03-06 15:14:08 -0800

    create has_many and belongs_to association classes

--- lib/03_associatable.rb
+++ lib/03_associatable.rb
@@ -10,23 +10,55 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    @class_name.constantize
   end
 
   def table_name
-    # ...
+    model_class.table_name
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    if options[:foreign_key]
+      @foreign_key = options[:foreign_key]
+    else
+      @foreign_key = "#{name}_id".to_sym
+    end
+
+    if options[:class_name]
+      @class_name = options[:class_name]
+    else
+      @class_name = name.to_s.camelize
+    end
+
+    if options[:primary_key]
+      @primary_key = options[:primary_key]
+    else
+      @primary_key = :id
+    end
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    if options[:foreign_key]
+      @foreign_key = options[:foreign_key]
+    else
+      @foreign_key = "#{self_class_name}Id".underscore.to_sym
+    end
+
+    if options[:class_name]
+      @class_name = options[:class_name]
+    else
+      @class_name = name.to_s.camelize.singularize
+    end
+
+    if options[:primary_key]
+      @primary_key = options[:primary_key]
+    else
+      @primary_key = :id
+    end
   end
 end
 
