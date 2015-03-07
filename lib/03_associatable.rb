fa315407ffaa8f7b4b8a9401aa63539916366501
Tripp Davis <trippdavis3@gmail.com>
2015-03-06 21:57:08 -0800

    add have one through method

--- lib/03_associatable.rb
+++ lib/03_associatable.rb
@@ -67,6 +67,8 @@ module Associatable
   def belongs_to(name, options = {})
     options = BelongsToOptions.new(name, options)
 
+    assoc_options[name] = options
+
     define_method("#{name}") do
       foreign_key = self.send(options.foreign_key)
       model_class = options.model_class
@@ -86,7 +88,7 @@ module Associatable
   end
 
   def assoc_options
-    # Wait to implement this in Phase IVa. Modify `belongs_to`, too.
+    @assoc_options ||= {}
   end
 end
 
--- lib/04_associatable2.rb
+++ lib/04_associatable2.rb
@@ -1,10 +1,26 @@
 require_relative '03_associatable'
-
+require 'byebug'
 # Phase IV
 module Associatable
   # Remember to go back to 04_associatable to write ::assoc_options
 
   def has_one_through(name, through_name, source_name)
-    # ...
+    define_method(name) do
+      through_options = self.class.assoc_options[through_name]
+      source_options = through_options.model_class.assoc_options[source_name]
+      result = DBConnection.execute(<<-SQL, self.attributes["#{through_options.foreign_key}".to_sym])
+        SELECT
+          a.*
+        FROM
+          #{source_options.table_name} AS a
+        JOIN
+          #{through_options.table_name} AS b
+        ON b.#{source_options.foreign_key} = a.#{source_options.primary_key}
+        WHERE
+          b.#{through_options.primary_key} = ?
+      SQL
+
+      source_options.class_name.constantize.new(result[0])
+    end
   end
 end
